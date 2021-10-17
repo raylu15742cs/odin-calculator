c72b7b45f318210540ef764d3716b5c266a7eced
raylu15742cs <raylu15742cs@gmail.com>
2021-10-16 22:40:38 -0700

    new-message

---
+++ script.js
@@ -0,0 +1,61 @@
+var x = document.getElementsByTagName("button");
+var y = document.getElementById('answers');
+let z = 0;
+let a = 0;
+let firstv = 0;
+let operator = 0;
+let secondv = 0;
+let result = 0;
+while (z <= 8 ){
+    let mess = x.item(z).innerHTML;
+    x.item(z).addEventListener("click",function(){
+        if(!isNaN(mess)){
+            y.innerHTML += mess;
+        };
+    });
+    //document.getElementById('answer').innerHTML += x.item(z).innerHTML;
+    console.log(x.item(z).innerHTML);
+    z += 1;
+}
+x.item(9).addEventListener('click',function(){
+    empty();
+})
+x.item(10).addEventListener('click',function() {
+    firstv = y.innerHTML;
+    operator = 1;
+    empty();
+})
+x.item(11).addEventListener('click',function() {
+    firstv = y.innerHTML;
+    operator = 2;
+    empty();
+})
+x.item(12).addEventListener('click',function() {
+    firstv = y.innerHTML;
+    operator = 3;
+    empty();
+})
+x.item(13).addEventListener('click',function() {
+    firstv = y.innerHTML;
+    operator = 4;
+    empty();
+})
+const empty = function(){
+    y.innerHTML = "";
+}
+
+x.item(14).addEventListener('click',function(){
+    secondv = y.innerHTML;
+    if(operator == 1){
+        result = Number(firstv) + Number(secondv);
+    } else if (operator == 2) {
+        result = Number(firstv) - Number(secondv); 
+    } else if (operator == 3) {
+        result = Number(firstv) * Number(secondv);
+    } else if (operator == 4) {
+        result = Number(firstv) / Number(secondv);
+    } else {
+        y.innerHTML = " u stupid"
+    }
+    y.innerHTML = result;
+})
